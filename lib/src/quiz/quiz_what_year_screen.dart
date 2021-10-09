4b1e5a585fb89e219b1495674a68ed31c6d1cf9f
Sergey Pavlov <theboringdevelopers@gmail.com>
2021-10-09 10:21:44 +0300

    Remove trash lib

--- lib/src/quiz/quiz_what_year_screen.dart
+++ lib/src/quiz/quiz_what_year_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_sliders/sliders.dart';
 import 'package:vtb_hack_mobile/src/common_widgets/full_width_button.dart';
-import 'package:vtb_hack_mobile/src/game/game_home_screen.dart';
 import 'package:vtb_hack_mobile/src/quiz/quiz_portfel_screen.dart';
 import 'package:vtb_hack_mobile/src/settings/size_config.dart';
 
--- lib/src/wrapper.dart
+++ lib/src/wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vtb_hack_mobile/src/quiz/quiz_home_screen.dart';
 import 'package:vtb_hack_mobile/src/settings/size_config.dart';
-import 'package:vtb_hack_mobile/src/video_tips/video_tips_screen.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
